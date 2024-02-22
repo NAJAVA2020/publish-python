f20db7918940af9508d2db83cf7cdfdb3f769541
NAJAVA2020 <elkanamutavi@gmail.com>
2024-02-22 10:42:40 +0000

    IOT and WSN development

---
+++ publish_python.py
@@ -0,0 +1,27 @@
+import serial
+
+# Define the serial port and baud rate
+serial_port = 'COM7'  # Change this to match your Arduino's serial port
+baud_rate = 9600
+
+# Open the serial port
+ser = serial.Serial(serial_port, baud_rate)
+
+# Open a file to write the received messages
+file_path = 'arduino_messages.txt'
+with open(file_path, 'w', encoding='UTF-8') as file:
+    while True:
+        # Read message from the serial port
+        message = ser.readline().decode().strip()
+
+        # Print the received message
+        print(message)
+
+        # Write the message to the file
+        file.write(message + '\n')
+
+        # Flush the file to ensure data is written immediately
+        file.flush()
+
+# Close the serial port
+ser.close()
